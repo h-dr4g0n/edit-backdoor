ea5c0b8c3af79c729c0f107c5bac3cc7a7bed1d2
h-dr4g0n <33449999+h-dr4g0n@users.noreply.github.com>
2018-01-07 17:01:39 +0700

    install tool linux

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install git
+git config --global user.name "h-dr4g0n"
+git config --global user.email "dragonmatrix2506@gmail.com"
+cat ~/.gitconfig
+
+export apktool_version=2.3.1
+sudo -E sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_$apktool_version.jar -O /usr/local/bin/apktool.jar'
+sudo chmod +r /usr/local/bin/apktool.jar
+sudo sh -c 'wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O /usr/local/bin/apktool'
+sudo chmod +x /usr/local/bin/apktool
