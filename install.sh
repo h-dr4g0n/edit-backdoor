c6f5796b5c47de8d247c2f306c96aa458fd0e539
h-dr4g0n <33449999+h-dr4g0n@users.noreply.github.com>
2018-01-07 17:13:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+./msfinstall
 sudo apt-get install git
 git config --global user.name "h-dr4g0n"
 git config --global user.email "dragonmatrix2506@gmail.com"
@@ -12,3 +13,6 @@ sudo -E sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_
 sudo chmod +r /usr/local/bin/apktool.jar
 sudo sh -c 'wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O /usr/local/bin/apktool'
 sudo chmod +x /usr/local/bin/apktool
+#apktool
+apktool d -f -o shopee com.shopee.vn-83.apk
+apktool d -f -o Duolingo Duolingo.apk
